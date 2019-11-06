e4786a1a07d4e8439120a8cecf6d6963b2362d58
repair123 <39139015+repair123@users.noreply.github.com>
2019-11-06 11:57:57 +0900

    Add files via upload

---
+++ 1106/NewKey.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class NewKey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+  transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Horizontal")*30.0f*Time.deltaTime,new Vector3(0.0f,0.0f,1.0f));
+  transform.rotation *= Quaternion.AngleAxis(Input.GetAxis("Vertical")*30.0f*Time.deltaTime,new Vector3(1.0f,0.0f,0.0f));
+
+ 
+    }
+}
